942f2d8a4def6b8d7e84d571f113e5707a44218f
Andrey231 <osad.andrey@yandex.ru>
2022-11-01 18:58:36 +0300

    update

--- Diplome/Assets/Scripts/Bullet.cs
+++ Diplome/Assets/Scripts/Bullet.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject particlecollision;
     private void OnCollisionEnter2D()
     {
+        Instantiate(particlecollision, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
