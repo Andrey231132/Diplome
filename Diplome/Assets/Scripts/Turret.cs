e31ae12ae3a0b14b904bdc2e70e67d21856b8e31
Andrey231 <osad.andrey@yandex.ru>
2022-11-03 11:27:17 +0300

    tryaddturret

---
+++ Diplome/Assets/Scripts/Turret.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : BaseEnemy
+{
+    [SerializeField]
+    private float distance;
+    [SerializeField]
+    private float seeradius;
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private Transform turretgun;
+    [SerializeField]
+    private Transform aim;
+
+    private Vector2 direction;
+    private bool isdetected;
+    private void Start()
+    {
+        StartCoroutine(IsDetected());
+    }
+    private void Update()
+    {
+        CheckHealth();
+        SpawnSeeRadius();
+    }
+    public override void Die()
+    {
+        Instantiate(partical, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+    private IEnumerator IsDetected()
+    {
+        while (true)
+        {
+            if (isdetected)
+            {
+                Shoot();
+                yield return new WaitForSeconds(speedfire);
+            }
+            yield return null;
+        }
+    }
+    private void Shoot()
+    {
+        GameObject _bullet = Instantiate(bullet, aim.position, turretgun.rotation);
+        _bullet.GetComponent<Rigidbody2D>().AddForce(aim.transform.up * speedbulet);
+        Destroy(_bullet, 2f);
+    }
+    private void SpawnSeeRadius()
+    {
+        direction = player.position - transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position,direction, seeradius);
+        if(hit && hit.collider.gameObject.GetComponent<PlayerController>())
+        {
+            turretgun.up = direction;
+            isdetected = true;
+        }
+        else
+        {
+            isdetected = false;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+       
+    }
+}
