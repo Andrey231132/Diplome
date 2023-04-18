4a520404d6a23ab3e10f1c2f82ff60a4e9d8d000
Andrey231 <osad.andrey@yandex.ru>
2023-04-18 19:05:35 +0300

    update

---
+++ Diplome/Assets/Scripts/Shrine1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrine1 : MonoBehaviour
+{
+    [SerializeField]
+    private Element element;
+    [SerializeField]
+    private Sprite sprite_element;
+    [SerializeField]
+    private Transform gameObject_element;
+
+    private void Awake()
+    {
+        gameObject_element.GetComponent<SpriteRenderer>().sprite = sprite_element;
+    }
+}
+enum Element
+{
+    Grass,
+    Fire,
+    Stone
+}
