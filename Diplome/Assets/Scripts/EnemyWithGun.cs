8700bf834d1b87903f24ac79ffc03d1ee6c67423
Andrey231 <osad.andrey@yandex.ru>
2023-04-30 16:49:18 +0300

    update

--- Diplome/Assets/Scripts/EnemyWithGun.cs
+++ Diplome/Assets/Scripts/EnemyWithGun.cs
@@ -109,6 +109,10 @@ public class EnemyWithGun : BaseEnemy
                 decectplayer = false;;
             }
         }
+        if(player = null)
+        {
+            decectplayer = false;
+        }
     }
     private IEnumerator Shooting()
     {
