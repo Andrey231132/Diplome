5ea8bfbf0e2ce22a6d7fc355e23ea3ae5d213064
Andrey231 <osad.andrey@yandex.ru>
2023-05-01 19:23:50 +0300

    update

--- Diplome/Assets/Scripts/Trap.cs
+++ Diplome/Assets/Scripts/Trap.cs
@@ -10,7 +10,7 @@ public class Trap : MonoBehaviour
     private int damage=1;
 
     private bool isactive;
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnCollisionStay2D(Collision2D col)
     {
         if (col.gameObject.GetComponent<PlayerController>() && !isactive)
         {
