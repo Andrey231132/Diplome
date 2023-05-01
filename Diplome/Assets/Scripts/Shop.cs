8886f97db6a0060678774832c9986b0dc2b44ea7
Andrey231 <osad.andrey@yandex.ru>
2023-05-01 14:47:45 +0300

    update

--- Diplome/Assets/Scripts/Shop.cs
+++ Diplome/Assets/Scripts/Shop.cs
@@ -6,27 +6,36 @@ using UnityEngine.UI;
 public class Shop : MonoBehaviour
 {
     public Text moneystext;
+    [SerializeField]
+    private int max_level_update = 3;
+
+    private int current_speed_update;
+    private int current_timerecord_update;
+    private int current_damage_update;
     public void Timerecord()
     {
-        if (GameManager.Money() > 0)
+        if (GameManager.Money() > 0 && current_timerecord_update !< max_level_update)
         {
             GameManager.timerecord -= 0.1f;
+            current_timerecord_update++;
             GameManager.Buy(1);
         }
     }
     public void Speed()
     {
-        if (GameManager.Money() > 0)
+        if (GameManager.Money() > 0 && current_speed_update! < max_level_update)
         {
             GameManager.speed += 0.05f;
+            current_speed_update++;
             GameManager.Buy(1);
         }
     }
     public void Damage()
     {
-        if (GameManager.Money() > 0)
+        if (GameManager.Money() > 0 && current_damage_update! < max_level_update)
         {
             GameManager.damage += 1;
+            current_damage_update++;
             GameManager.Buy(1);
         }
     }
