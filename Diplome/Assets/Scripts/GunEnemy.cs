24d67067c9856252f7128b709db3c275985bcfad
Andrey231 <osad.andrey@yandex.ru>
2023-05-13 18:38:04 +0300

    update

--- Diplome/Assets/Scripts/GunEnemy.cs
+++ Diplome/Assets/Scripts/GunEnemy.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class GunEnemy : MonoBehaviour
 {
-    [SerializeField]
     private StateEnemy _stateenemy;
+
+    [SerializeField]
+    private Element _element;
 
     [SerializeField]
     private Transform A_place;
@@ -13,12 +15,24 @@ public class GunEnemy : MonoBehaviour
     private Transform B_place;
     [SerializeField]
     private Transform _raycastplace;
+    [SerializeField]
+    private GameObject _particaldamage;
+    [SerializeField]
+    private GameObject _dropmoney;
+    [SerializeField]
+    private GameObject _shootbullet;
+    [SerializeField]
+    private Transform aim;//Откуда вылетают пули
 
     private Transform _currentplace;
     private Transform _player;
 
     [SerializeField]
     private float _speed;
+    [SerializeField]
+    private float _bulletspeed;
+    [SerializeField]
+    private float _health;
     [SerializeField]
     private float _timebetwenshoot;
     [SerializeField]
@@ -29,38 +43,44 @@ public class GunEnemy : MonoBehaviour
     private float _atackplayerdistance;
 
     private Animator anim;
+    private AudioSource audio;
 
     //Дополнителтьное_помогающее
     private bool Isdetectplayer;
     private void Start()
     {
         _currentplace = A_place;
+
         anim = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+
+        SetAudio();
     }
     private void Update()
     {
         Logic();
         CheckEnemyState();
+        CheckHealth();
     }
     private void Logic()
     {
-        if(CheckPlayer())
+        if (CheckPlayer())
         {
             _stateenemy = StateEnemy.Follow;
         }
-        else if(Isdetectplayer)//Если мы видели игрока,но он скрылся из ввиду
+        else if (Isdetectplayer)//Если мы видели игрока,но он скрылся из ввиду
         {
             _stateenemy = StateEnemy.Stay;
         }
-        else if(_stateenemy != StateEnemy.Stay)
+        else if (_stateenemy != StateEnemy.Stay)
         {
             _stateenemy = StateEnemy.Patrule;
         }
-        if(CheckPlayer() && Vector2.Distance(new Vector2(_player.position.x, 0), new Vector2(gameObject.transform.position.x, 0)) <= _atackplayerdistance)
+        if (CheckPlayer() && Vector2.Distance(new Vector2(_player.position.x, 0), new Vector2(gameObject.transform.position.x, 0)) <= _atackplayerdistance)
         {//Если враг видет игрока и расстояние между ними равно растояние атаки то враг атакует
             _stateenemy = StateEnemy.Shoot;
         }
-        if(_stateenemy == StateEnemy.Patrule && Vector2.Distance(new Vector2(_currentplace.position.x, 0), new Vector2(gameObject.transform.position.x, 0)) <= 0.5f)
+        if (_stateenemy == StateEnemy.Patrule && Vector2.Distance(new Vector2(_currentplace.position.x, 0), new Vector2(gameObject.transform.position.x, 0)) <= 0.5f)
         {//Если враг патрилирует и если он дошел до точки то меняем ему точку
             _stateenemy = StateEnemy.Stay;
             if (_currentplace == A_place)
@@ -75,38 +95,38 @@ public class GunEnemy : MonoBehaviour
     }
     private void CheckEnemyState()
     {
-        switch(_stateenemy)
+        switch (_stateenemy)
         {
             case StateEnemy.Follow:
-            {
+                {
                     Following();
                     StopCoroutine(Staying());
                     StopCoroutine(Shooting());
                     break;
-            }
+                }
             case StateEnemy.Stay:
-            {
+                {
                     StartCoroutine(Staying());
                     break;
-            }
+                }
             case StateEnemy.Shoot:
-            {
+                {
                     StartCoroutine(Shooting());
                     StopCoroutine(Staying());
                     break;
-            }
+                }
             case StateEnemy.Patrule:
-            {
+                {
                     StopCoroutine(Shooting());
                     StopCoroutine(Staying());
                     Patruling();
                     break;
-            }
+                }
         }
     }
     private bool CheckPlayer()
     {
-        RaycastHit2D hit = Physics2D.Raycast(_raycastplace.position,transform.right,_seeplayerdistance);
+        RaycastHit2D hit = Physics2D.Raycast(_raycastplace.position, transform.right, _seeplayerdistance);
         if (hit)
         {
             if (hit.collider.gameObject.GetComponent<PlayerController>() || hit.collider.gameObject.GetComponent<shild1>())
@@ -115,22 +135,22 @@ public class GunEnemy : MonoBehaviour
                 _player = hit.collider.gameObject.transform;
                 return true;
             }
-            else 
+            else
             {
                 return false;
             }
         }
         return false;
-    }     
+    }
     private void OnDrawGizmos()
     {
         Gizmos.DrawRay(_raycastplace.position, transform.right * _seeplayerdistance);
     }
     private void Patruling()
     {
-        if(transform.position.x > _currentplace.position.x)
+        if (transform.position.x > _currentplace.position.x)
         {
-            transform.position -= new Vector3(_speed,0) * Time.deltaTime;
+            transform.position -= new Vector3(_speed, 0) * Time.deltaTime;
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
         if (transform.position.x < _currentplace.position.x)
@@ -142,7 +162,7 @@ public class GunEnemy : MonoBehaviour
     }
     private void Following()
     {
-        if(_player && Vector2.Distance(_player.position, gameObject.transform.position) > _atackplayerdistance)
+        if (_player && Vector2.Distance(_player.position, gameObject.transform.position) > _atackplayerdistance)
         {
             if (gameObject.transform.position.x >= _player.position.x)
             {
@@ -158,7 +178,7 @@ public class GunEnemy : MonoBehaviour
     }
     private IEnumerator Staying()
     {
-        anim.SetBool("Run",false);
+        anim.SetBool("Run", false);
         yield return new WaitForSeconds(_timestay);
         Isdetectplayer = false;
         _stateenemy = StateEnemy.Patrule;
@@ -174,8 +194,71 @@ public class GunEnemy : MonoBehaviour
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
         anim.SetBool("Run", false);
+        SpawnBullet();
         yield return new WaitForSeconds(_timebetwenshoot);
-        
+    }
+    private void SpawnBullet()
+    {
+        GameObject _bullet = Instantiate(_shootbullet, aim.position, Quaternion.identity);
+        if (transform.eulerAngles.y == 0)
+        {
+            _bullet.GetComponent<Bullet>().SetBulletSpeed(_bulletspeed);
+        }
+        else if (transform.eulerAngles.y == 180)
+        {
+            _bullet.GetComponent<Bullet>().SetBulletSpeed(-_bulletspeed);
+        }
+    }
+    private void SetAudio()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.volume = PlayerPrefs.GetFloat("musicvalue");
+    }
+    public void GetDamage(int damage)
+    {
+        audio = GetComponent<AudioSource>();
+        _health -= damage;
+        Instantiate(_particaldamage, transform.position, Quaternion.identity);
+        Instantiate(_dropmoney, transform.position, Quaternion.identity);
+        audio.Play();
+    }
+    private void CheckHealth()
+    {
+        if (_health <= 0)
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+        Instantiate(_particaldamage, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<Bullet>())
+        {
+            Bullet bullet = col.gameObject.GetComponent<Bullet>();
+            if (col.gameObject.tag == "PlayerBullet" && !CheckElementBullet(bullet))
+            {
+                GetDamage(bullet.GetDamage());
+            }
+            else if (col.gameObject.tag == "PlayerBullet" && CheckElementBullet(bullet))
+            {
+                GetDamage(bullet.GetDamage() * 2);
+            }
+        }
+    }
+    private bool CheckElementBullet(Bullet bul)
+    {
+        if (bul.GetElement() == _element)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
 public enum StateEnemy
